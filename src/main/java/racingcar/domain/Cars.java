972f9fcc635e1e76c903dbab8d65dcef99886d08
hyun0618 <jhh48654@gmail.com>
2025-10-27 23:26:21 +0900

    feat: print final winners after all rounds

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,32 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Cars {
+    private final List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public List<Car> getCars() {
+        return Collections.unmodifiableList(cars);
+    }
+
+    public List<String> getWinners() {
+        int maxPosition = cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+
+        List<String> winners = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getPosition() == maxPosition) {
+                winners.add(car.getName());
+            }
+        }
+        return winners;
+    }
+}
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,6 +2,7 @@ package racingcar.view;
 
 import racingcar.domain.Car;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OutputView {
 
@@ -15,5 +16,11 @@ public class OutputView {
 
     public static void printResultTitle() {
         System.out.println("\n실행 결과");
+    }
+
+    public static void printWinners(List<String> winners) {
+        String result = winners.stream()
+                .collect(Collectors.joining(", "));
+        System.out.println("최종 우승자 : " + result);
     }
 }
