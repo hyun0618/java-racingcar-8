aebb05ebe722391bb3ae2ef745f4ddd19ab18b86
hyun0618 <jhh48654@gmail.com>
2025-10-27 23:16:35 +0900

    feat: add car movement logic based on random number

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,29 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private static final int MOVE_THRESHOLD = 4;
+
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void move() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        if (randomNumber >= MOVE_THRESHOLD) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
